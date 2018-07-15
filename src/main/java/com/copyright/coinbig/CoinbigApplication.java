be7c74f7eeb2fc522db8e5c8d2d1f10ebff77b4d
cao.yin <tianshencaoyin@163.com>
2018-07-15 21:20:17 +0800

    first commit

---
+++ src/main/java/com/copyright/coinbig/CoinbigApplication.java
@@ -0,0 +1,12 @@
+package com.copyright.coinbig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinbigApplication.class, args);
+    }
+}
